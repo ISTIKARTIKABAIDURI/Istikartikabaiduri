74cda77a45407b0c23dae0512de0287a10072f99
ISTIKARTIKABAIDURI <istikartika230304@gmail.com>
2024-11-29 09:54:19 +0700

    Add files via upload

---
+++ kuis2.php
@@ -0,0 +1,36 @@
+<?php
+// Soal a)
+echo "Soal a) \n";
+$i = 1;
+while ($i <= 16) {
+  echo $i . " ";
+  $i += 5;
+}
+echo "\n\n"; 
+
+// Soal b)
+echo "Soal b) \n";
+$i = 10;
+while ($i >= 0) {
+  echo $i . " ";
+  $i--;
+}
+echo "\n\n";
+
+// Soal c)
+echo "Soal c) \n";
+$i = 30;
+do {
+  echo $i . " ";
+  $i -= 3;
+} while ($i >= 0);
+echo "\n\n";
+
+// Soal d)
+echo "Soal d) \n";
+$i = 10;
+do {
+  echo $i . " ";
+  $i -= 2;
+} while ($i >= 0);
+?>
